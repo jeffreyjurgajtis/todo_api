565e46420eff236e0167359fa87e6d9ac6fc3cb6
Jeffrey Jurgajtis <jeffreyjurgajtis@gmail.com>
2015-11-02 10:21:39 -0600

    Replace date time string values with echo date time structs

--- test/models/todo_test.exs
+++ test/models/todo_test.exs
@@ -4,15 +4,15 @@ defmodule TodoApi.TodoTest do
   alias TodoApi.Todo
   alias TodoApi.Repo
 
-  test "changeset valid? returns true when content and user_id are present " do
-    attrs = %{completed_at: "2010-04-17 14:00:00", content: "hey", user_id: 1}
+  test "changeset valid? returns true when content and user_id are present" do
+    attrs = %{content: "content", user_id: 1}
     changeset = Todo.changeset(%Todo{}, attrs)
 
     assert changeset.valid?
   end
 
   test "validations require content to be present" do
-    attrs = %{completed_at: "2010-04-17 14:00:00", content: ""}
+    attrs = %{content: ""}
     changeset = Todo.changeset(%Todo{}, attrs)
     {message, _count} = changeset.errors[:content]
 
@@ -20,7 +20,7 @@ defmodule TodoApi.TodoTest do
   end
 
   test "insert ensures user_id is associated with existing user" do
-    attrs = %{completed_at: "2010-04-17 14:00:00", content: "hey", user_id: 123}
+    attrs = %{completed_at: Ecto.DateTime.local, content: "hey", user_id: 123}
     {:error, changeset} = %Todo{} |> Todo.changeset(attrs) |> Repo.insert
 
     assert changeset.errors[:user] == "does not exist"
